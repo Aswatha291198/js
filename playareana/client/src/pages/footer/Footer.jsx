23cc5807c38dea2da1e6337723286469e6cbd0a9
Aswatha291198 <aswatha291198@gmail.com>
2025-08-28 23:04:08 +0530

    commited

---
+++ playareana/client/src/pages/footer/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <>
+    <section className='footer-cont'>
+        <footer className='footer-disp'>
+            <div className='footer-logo'>
+            <ul>
+                <span>company</span>
+                <li>home</li>
+                <li>contact</li>
+                <li>parter with us</li>
+                <li>About us</li>
+                
+            </ul>
+            <ul>
+                <span>Social</span>
+                <li>Instagram</li>
+                <li>Facebook</li>
+                <li>Linkedin</li>
+            </ul>
+            <ul>
+                <span>Privacy and Terms</span>
+                <li>Faq </li>
+                <li>Privacy Policy</li>
+                <li>    </li>
+                <li>
+                    CANCELLATION POLICY
+                </li>
+            </ul>
+            </div>
+            
+        </footer>
+
+    </section>
+    </>
+  )
+}
+
+export default Footer
--- playareana/client/src/pages/home/Home.jsx
+++ playareana/client/src/pages/home/Home.jsx
@@ -1,74 +1,187 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './home.css'
 import kick from '../home/kick.png'
-import {useSelector,useDispatch} from 'react-redux'
-import {hideLoading,showLoading} from  '../../../redux/slice/userSlice'
+import { useSelector, useDispatch } from 'react-redux'
+import { hideLoading, showLoading } from '../../../redux/slice/userSlice'
 import { getAllTurf } from '../../api/turf'
 import group from '../../assets/group.png'
 import support from '../../assets/support.png'
+import trophy from '../../assets/trophy.png'
+import coach from '../../assets/coach.png'
+import { getAllGame } from '../../api/game'
+import { message } from 'antd'
+import Footer from '../footer/Footer'
+
 
 const Home = () => {
-  const[turfs,setTurs]=useState(null)
-  const dispatch=useDispatch()
-  const {user}=useSelector(store=>store.users)
+  const [turfs, setTurs] = useState(null)
+  const dispatch = useDispatch()
+  const { user } = useSelector(store => store.users)
+  const [sport, setSports] = useState(null)
+
+  const getSport = async () => {
+    try {
+      dispatch(showLoading())
+      const response = await getAllGame()
+      if (response.success) {
+        const allgames = response.data
+        setSports(allgames.map((game) => {
+          return {
+            ...game,
+            key: `game${game._id}`
+          }
+        }))
+      }
+      else {
+        message.error(response.message)
+      }
+      dispatch(hideLoading())
+    } catch (error) {
+      dispatch(hideLoading())
+      console.log(error.message);
+    }
+  }
+  useEffect(() => {
+    getSport()
+  }, [])
 
   return (
     <>
-   <main className='container'>
-    <section className='section-cont'>
-      <div className='content-div'>
-         <div className='space-div'>
-          <div className='first-div'>
-          <div className='blocking'></div>
-          
-          <h1 className='content-info'>Explore Playing Communiteis</h1>
-          <p className='content-para'>Connect, play, and grow with like-minded sports enthusiasts around you</p>
-        </div>
-         </div>
-        
-        <div className='second-div'>
-          <div className='second-div-img'>
-            <img src={kick} alt="kick" className='kick' />
+      <main className='container'>
+        <section className='section-cont'>
+          <div className='content-div'>
+            <div className='space-div'>
+              <div className='first-div'>
+                <div className='blocking'></div>
+
+                <h1 className='content-info'>Explore Playing Communiteis</h1>
+                <p className='content-para'>Connect, play, and grow with like-minded sports enthusiasts around you</p>
+              </div>
+            </div>
+
+            <div className='second-div'>
+              <div className='second-div-img'>
+                <img src={kick} alt="kick" className='kick' />
+              </div>
+            </div>
           </div>
+        </section>
+        <section className='tournement-div-home'>
+          {user && user.role === 'player' && (
+            <div className='div-wrap'>
+              <div className='lead-name'>
+                <h1 className='be'>Be the Game <span className='color'>Changer</span></h1>
+              </div>
+            </div>
+          )}
+          <div className='content-row'>
+            {user && user.role === 'player' && (
+              <>
+                <div className='start'>
+                  <div className='start-div'>
+                    <div className='start-img'>
+                      <div className='trophy'>
+                        <span className='tro-span'><img src={trophy} alt="tro" className='tro' /></span>
+                      </div>
+                    </div>
+                    <div className='start-name'>
+                      <h2 className='kick'>Kickstart a Tournament  </h2>
+                      <p className='kick-p'>Create your own league and crown the champions</p>
+                    </div>
+                    <div className='start-btn'>
+                      <button className='btn-str'>Start</button>
+                    </div>
+
+                  </div>
+                </div>
+                <div className='start'>
+                  <div className='start-div'>
+                    <div className='start-img'>
+                      <div className='trophy '>
+                        <span><img src={coach} alt="c" className='coach' /></span>
+                      </div>
+                      <div>
+
+                      </div>
+                    </div>
+                    <div className='start-name'>
+                      <h2 className='kick-h'>Step In as a Trainer</h2>
+                      <p className='kick-t'>Turn passion into guidance, and players into legends.</p>
+                    </div>
+                    <div className='start-btn'>
+                      <button className='btn-str'>Start</button>
+                    </div>
+                  </div>
+
+                </div>
+              </>
+            )}
+            {user && user.role === 'owner' &&(
+                    <div className='div-wrap'>
+        <div className='lead-name'>
+          <h1 className='be'>Own a Turf? Manage it  <span className='color'>Smarter</span></h1>
         </div>
       </div>
-    </section>
+            )}
+            {user && user.role === 'owner' && (
 
-  
-      <section className='choose-container'>
-        {user.role==='player' && (
-          <>
-        <div className='connect'>
-          <div className='connect-img'>
-            <img src={group} alt="gropu" className='group' />
-          </div>
-          <div className='connect-wrap'>
-            <span className='connect-text'>Easy To Connect</span>
-          </div>
-          <div className='connect-texts-div'>
-            <h3 className='connect-texts'>Book, play, and enjoy without boundaries</h3>
-          </div>
-        </div>
+              <>
+                <div className='start'>
+                  <div className='start-div'>
+                    <div className='start-img'>
+                      <div className='trophy'>
+                        <span className='tro-span'><img src={trophy} alt="tro" className='tro' /></span>
+                      </div>
+                    </div>
+                    <div className='start-name'>
+                      <h2 className='kick'>List Your Turf  </h2>
+                      <p className='kick-p'>Showcase your venue and attract more players effortlessly</p>
+                    </div>
+                    <div className='start-btn'>
+                      <button className='btn-str'>Start</button>
+                    </div>
 
-        <div className='connect'>
-          <div className='connect-img'>
-            <img src={support} alt="gropu" className='group' />
+                  </div>
+                </div>
+                <div className='start'>
+                  <div className='start-div'>
+                    <div className='start-img'>
+                      <div className='trophy '>
+                        <span><img src={coach} alt="c" className='coach' /></span>
+                      </div>
+                      <div>
+
+                      </div>
+                    </div>
+                    <div className='start-name'>
+                      <h2 className='kick-h'>Manage with Ease</h2>
+                      <p className='kick-t'>Control your turf hassle-free, all in one place</p>
+                    </div>
+                    <div className='start-btn'>
+                      <button className='btn-str'>Start</button>
+                    </div>
+                  </div>
+
+                </div>
+              </>
+            )}
           </div>
-          <div className='connect-wrap'>
-            <span className='connect-text'>Customer Support</span>
-          </div>
-          <div className='connect-texts-div'>
-            <h3 className='connect-texts-supports'>Reliable support to assist you at every step</h3>
-          </div>
+        </section>
+        <div className='sports-cont'>
+          {sport && sport.map((game) => {
+            return (
+              <div
+                key={game._id}
+                className='sport-wrapper'
+              >
+                <img src={game.image} alt="game" />
+                <span className='game-name'>{game.name}</span>
+              </div>
+            )
+          })}
         </div>
-        <div className='connect'>
-        </div>
-          
-          </>
-        )}
-      </section>
-  
-   </main>
+<Footer/>
+      </main>
     </>
   )
 }
